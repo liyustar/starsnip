3710e50a10ca719e5783f19a07be2e3c3dd7f64f
liyustar <liyustar@gmail.com>
2013-08-29 09:38:49 -0700

    add config dir

---
+++ config/gitconf.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+git config --global user.name "liyustar"
+git config --global user.email "liyustar@gmail.com"
+git config --global color.ui true
+git config --global alias.co checkout
+git config --global alias.ci commit
+git config --global alias.st status
+git config --global alias.br branch
+# git config --global core.editor "mate -w" # set editor use textmate
+git config -l # list all config
+
