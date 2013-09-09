02669226ad1bbe1d5499d6d624f2ee8ddf64e906
liyustar <liyustar@gmail.com>
2013-09-09 08:34:31 -0700

    add push.default to gitconf.sh

--- config/gitconf.sh
+++ config/gitconf.sh
@@ -7,6 +7,8 @@ git config --global alias.co checkout
 git config --global alias.ci commit
 git config --global alias.st status
 git config --global alias.br branch
+# git config --global push.default matching
+# git config --global push.default simple
 # git config --global core.editor "mate -w" # set editor use textmate
 git config -l # list all config
 
