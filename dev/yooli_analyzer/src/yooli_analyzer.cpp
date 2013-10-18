bd96652cc175905b81b67a768c99f44050c402e5
liyustar <liyustar@gmail.com>
2013-10-18 23:01:03 +0800

    set other header, can get gzip file

--- dev/yooli_analyzer/src/yooli_analyzer.cpp
+++ dev/yooli_analyzer/src/yooli_analyzer.cpp
@@ -4,6 +4,14 @@
 #include "lyxHttp.h"
 using namespace std;
 
+void addCommonHeader(lyx::Http &http) {
+	http.addHeader("Accept", "text/plain, */*; q=0.01");
+	http.addHeader("User-Agent", "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/30.0.1599.69 Safari/537.36");
+	http.addHeader("Content-Type", "application/x-www-form-urlencoded; charset=UTF-8");
+	http.addHeader("Accept-Encoding", "gzip,deflate,sdch");
+	http.addHeader("Accept-Language", "zh-CN,zh;q=0.8");
+}
+
 int main() {
 	// string yooliUrl = "yttp://www.yooli.com";
 	// lyx::Url yooliUrl("https://github.com:443/");
@@ -12,9 +20,8 @@ int main() {
 	lyx::Http::initHttpAlgorithms();
 	lyx::Http yooliHttp(yooliUrl);
 	yooliHttp.setMethod("GET");
-//	yooliHttp.setTimeout(8000);
-//	yooliHttp.setUserAgent("");
-//	yooliHttp.setAccept("");
+	addCommonHeader(yooliHttp);
+
 
 	string response;
 	yooliHttp.getResponse(response);
@@ -24,14 +31,25 @@ int main() {
 	// cout << response;
 
 	lyx::Url loginUrl("https://www.yooli.com:443/secure/login.action");
-	lyx::Http::initHttpAlgorithms();
 	lyx::Http loginHttp(loginUrl);
 	loginHttp.setMethod("POST");
 	loginHttp.addParam("nickname", "XXX");
-	loginHttp.addParam("password", "YYY");
+	loginHttp.addParam("password", "XXX");
+	loginHttp.addParam("verifycode", "");
+	loginHttp.addParam("chkboxautologin", "false");
+	loginHttp.addHeader("Referer", "https://www.yooli.com/secure/login.jsp");
+	addCommonHeader(loginHttp);
 	
 	response.clear();
 	loginHttp.getResponse(response);
+
+	lyx::Url yooliUrl2("http://www.yooli.com:80");
+	lyx::Http login2Http(yooliUrl2);
+	login2Http.setMethod("GET");
+	addCommonHeader(login2Http);
+
+	response.clear();
+	login2Http.getResponse(response);
 	cout << response;
 
 	return 0;
