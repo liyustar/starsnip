eda22b787474679b80b12ca586a05e4c54827e75
liyustar <liyustar@gmail.com>
2013-10-25 00:16:55 +0800

    gitconfig add push.default and crlf about

--- config/gitconf.sh
+++ config/gitconf.sh
@@ -2,14 +2,17 @@
 
 git config --global user.name "liyustar"
 git config --global user.email "liyustar@gmail.com"
-git config --global color.ui true
+
 git config --global alias.co checkout
 git config --global alias.ci commit
 git config --global alias.st status
 git config --global alias.br branch
-git config --global core.editor vim
-# git config --global push.default matching
-# git config --global push.default simple
-# git config --global core.editor "mate -w" # set editor use textmate
-git config -l # list all config
 
+git config --global color.ui true
+git config --global core.editor vim
+git config --global core.autocrlf false
+git config --global core.safecrlf true
+
+git config --global push.default simple
+
+git config -l # list all config
